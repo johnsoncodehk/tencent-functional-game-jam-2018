038a98690b75a37e4bc119c294646d26063df816
Oliver Ye <wenzi1996@live.com>
2018-11-03 11:31:19 +0800

    Add script“AIInStraightOrbit”
    this script is used to control moving platforms.

---
+++ Assets/TencentFunctionalGameJam2018/Scripts/AIInStraightOrbit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIInStraightOrbit : MonoBehaviour {
+	public Vector3 StartPoint;
+	public Vector3 EndPoint;
+	public bool IsLoop;
+	public float Speed;
+	Vector3 _direction{
+		get{
+			return (EndPoint - StartPoint).normalized;
+		}
+	}
+	int _directionSign = 1;
+	float _minDistance = 0.1f;
+	// Use this for initialization
+	void Start () {
+		transform.position = StartPoint;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(StartPoint,EndPoint) <= _minDistance )
+			return;
+		if(!IsLoop )
+		{
+			if((Vector3.Distance(transform.position,EndPoint)<= _minDistance))
+			{
+				_directionSign = 0;
+			}
+		}else
+		if((Vector3.Distance(transform.position,EndPoint) <= _minDistance) && _directionSign>0)
+		{
+			_directionSign = -1;
+		}else
+		if((Vector3.Distance(transform.position,StartPoint) <= _minDistance) && _directionSign<0)
+		{
+			_directionSign = 1;
+		}
+		transform.Translate(_direction*Speed*_directionSign);
+	}
+}
