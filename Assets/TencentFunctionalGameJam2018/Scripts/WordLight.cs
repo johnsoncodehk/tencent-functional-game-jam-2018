cbc2fe28f1ea6da51d8a8c8d8b6c3ab1f5934a3f
johnsoncodehk <johnsoncodehk@gmail.com>
2018-11-03 10:55:32 +0800

    Add level 1 word

---
+++ Assets/TencentFunctionalGameJam2018/Scripts/WordLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordLight : MonoBehaviour
+{
+
+    public Transform character;
+    public Transform o;
+    public SpriteRenderer lightSprite;
+
+    void Update()
+    {
+        float d = Vector2.Distance(character.position, o.position);
+        lightSprite.color = new Color(1, 1, 1, 1 - d / 4);
+    }
+}
