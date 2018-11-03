a749ecefa296804ec119070f5ccb17a855987b8a
johnsoncodehk <johnsoncodehk@gmail.com>
2018-11-03 12:33:59 +0800

    Add layer scrolls

---
+++ Assets/TencentFunctionalGameJam2018/Scripts/LayerScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerScroll : MonoBehaviour
+{
+
+    [Range(0, 1)] public float p;
+
+    float startX;
+    float startCameraX;
+
+    void Awake()
+    {
+        startX = transform.localPosition.x;
+        startCameraX = Camera.main.transform.localPosition.x;
+    }
+    void LateUpdate() {
+        float cameraX = Camera.main.transform.localPosition.x;
+        float d = cameraX - startCameraX;
+        d *= p;
+        Vector3 pos = transform.localPosition;
+        pos.x = startX + d;
+        transform.localPosition = pos;
+    }
+}
