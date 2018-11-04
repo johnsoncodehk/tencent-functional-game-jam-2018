2e67c6ae5f4f818e9d3b2b68de6be4ce5c1ffc45
Oliver Ye <wenzi1996@live.com>
2018-11-05 04:24:09 +0800

    Fix Falling Down Problem

--- Assets/TencentFunctionalGameJam2018/Scripts/AIInStraightOrbit.cs
+++ Assets/TencentFunctionalGameJam2018/Scripts/AIInStraightOrbit.cs
@@ -7,37 +7,54 @@ public class AIInStraightOrbit : MonoBehaviour {
 	public Vector3 EndPoint;
 	public bool IsLoop;
 	public float Speed;
-	Vector3 _direction{
-		get{
+
+	new private Rigidbody2D rigidbody;
+	Vector3 _direction {
+		get {
 			return (EndPoint - StartPoint).normalized;
 		}
 	}
 	int _directionSign = 1;
 	float _minDistance = 0.1f;
 	// Use this for initialization
-	void Start () {
+	IEnumerator Start () {
 		transform.position = StartPoint;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Vector3.Distance(StartPoint,EndPoint) <= _minDistance )
-			return;
-		if(!IsLoop )
-		{
-			if((Vector3.Distance(transform.position,EndPoint)<= _minDistance))
-			{
-				_directionSign = 0;
+		rigidbody = GetComponent<Rigidbody2D>();
+		while (true) {
+			while (transform.position.x < Mathf.Max (StartPoint.x, EndPoint.x)) {
+				rigidbody.velocity = new Vector2 (Speed, 0);
+				yield return new WaitForEndOfFrame ();
 			}
-		}else
-		if((Vector3.Distance(transform.position,EndPoint) <= _minDistance) && _directionSign>0)
-		{
-			_directionSign = -1;
-		}else
-		if((Vector3.Distance(transform.position,StartPoint) <= _minDistance) && _directionSign<0)
-		{
-			_directionSign = 1;
+			while (transform.position.x > Mathf.Min (StartPoint.x, EndPoint.x)) {
+				rigidbody.velocity = new Vector2 (-Speed, 0);
+				yield return new WaitForEndOfFrame ();
+			}
+			yield return new WaitForEndOfFrame ();
 		}
-		transform.Translate(_direction*Speed*_directionSign);
 	}
 }
+
+// // Update is called once per frame
+// void Update () {
+// 	if(Vector3.Distance(StartPoint,EndPoint) <= _minDistance )
+// 		return;
+// 	if(!IsLoop )
+// 	{
+// 		if((Vector3.Distance(transform.position,EndPoint)<= _minDistance))
+// 		{
+// 			_directionSign = 0;
+// 		}
+// 	}else
+// 	if((Vector3.Distance(transform.position,EndPoint) <= _minDistance) && _directionSign>0)
+// 	{
+// 		_directionSign = -1;
+// 	}else
+// 	if((Vector3.Distance(transform.position,StartPoint) <= _minDistance) && _directionSign<0)
+// 	{
+// 		_directionSign = 1;
+// 	}
+// 	//transform.Translate(_direction*Speed*_directionSign);
+
+//     rigidbody.velocity = new Vector2(Speed*_directionSign,0);
+// }
+// }
