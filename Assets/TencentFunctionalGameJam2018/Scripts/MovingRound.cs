23d8113b7b7a70b1ff67c38adf597edeca941be1
johnsoncodehk <johnsoncodehk@gmail.com>
2018-11-05 01:14:56 +0800

    Fix 小云安全跳台

--- Assets/TencentFunctionalGameJam2018/Scripts/MovingRound.cs
+++ Assets/TencentFunctionalGameJam2018/Scripts/MovingRound.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class MovingRound : MonoBehaviour
 {
-       public Transform aroundPoint;//围绕的物体
+    public Transform aroundPoint;//围绕的物体
     public float angularSpeed;//角速度
     public float aroundRadius;//半径
+    public float angled;
 
-    private float angled;
-    void Start () {
-      
-        Vector3 p = aroundPoint.rotation * Vector3.forward * aroundRadius;
-        transform.position = new Vector3(p.x, aroundPoint.position.y, p.z);
+    new private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        // Vector3 p = aroundPoint.rotation * Vector3.forward * aroundRadius;
+        // transform.position = new Vector3(p.x, aroundPoint.position.y, p.z);
+
+        transform.position = aroundPoint.position;
     }
 
-    void Update () {
+    void FixedUpdate()
+    {
         angled += (angularSpeed * Time.deltaTime) % 360;//累加已经转过的角度
         float posX = aroundRadius * Mathf.Sin(angled * Mathf.Deg2Rad);//计算x位置
         float posY = aroundRadius * Mathf.Cos(angled * Mathf.Deg2Rad);//计算y位置
 
-        transform.position = new Vector3(posX, posY, 0) + aroundPoint.position;//更新位置
+        Vector3 targetPosition = new Vector3(posX, posY, 0) + aroundPoint.position;
+        Vector2 d = targetPosition - transform.position;
+        rigidbody.velocity = d;
     }
 
 }
