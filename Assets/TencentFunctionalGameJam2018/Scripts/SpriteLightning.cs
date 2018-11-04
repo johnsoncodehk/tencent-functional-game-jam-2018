7c42702e0f0813d68192055d689be02d848d73d8
johnsoncodehk <johnsoncodehk@gmail.com>
2018-11-04 11:59:06 +0800

    Add level 3 sprites

---
+++ Assets/TencentFunctionalGameJam2018/Scripts/SpriteLightning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLightning : MonoBehaviour
+{
+    public void StartLightning()
+    {
+        StartCoroutine(StartLightningCoroutine());
+    }
+    IEnumerator StartLightningCoroutine()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        float startTime = Time.time;
+        while (Time.time - startTime < 2)
+        {
+            int randomNum = Random.Range(1, 5);
+
+            for (int i = 0; i < randomNum; i++)
+            {
+                sprite.enabled = false;
+                yield return new WaitForSeconds(0.02f);
+                sprite.enabled = true;
+                yield return new WaitForSeconds(0.02f);
+            }
+
+            yield return new WaitForSeconds(Random.Range(0f, 1f));
+        }
+    }
+}
