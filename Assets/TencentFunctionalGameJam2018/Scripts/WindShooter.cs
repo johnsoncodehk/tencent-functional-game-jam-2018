d5607410f1db3211c7be7350af52549ddbfa9d97
johnsoncodehk <johnsoncodehk@gmail.com>
2018-11-06 16:56:54 +0800

    Add wind delay time setting

--- Assets/TencentFunctionalGameJam2018/Scripts/WindShooter.cs
+++ Assets/TencentFunctionalGameJam2018/Scripts/WindShooter.cs
@@ -6,6 +6,9 @@ public class WindShooter : MonoBehaviour
 {
     public static WindShooter instance;
     public GameObject wind;
+    public float minDelayTime = 0.3f;
+    public float maxDelayTime = 0.7f;
+
     void Awake()
     {
         instance = this;
@@ -19,7 +22,7 @@ public class WindShooter : MonoBehaviour
         Vector2 boxSize = GetComponent<BoxCollider2D>().size;
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(0.3f, 0.7f));
+            yield return new WaitForSeconds(Random.Range(minDelayTime, maxDelayTime));
             Vector2 pos = new Vector2(
                 Random.Range(transform.position.x - boxSize.x * 0.5f, transform.position.x + boxSize.x * 0.5f),
                 Random.Range(transform.position.y - boxSize.y * 0.5f, transform.position.y + boxSize.y * 0.5f)
